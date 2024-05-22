3a2db549d292c0738e9fbb672202bfef8233a9ca
George Crary <George Takumi Crary>
2024-05-21 22:21:21 -0700

    Add treeedbgen-souffle-tsql
    Like treeedb-tsql, this depends on local `tree-sitter-tsql = { path = "../../tree-sitter-tsql" }`.
    
    I haven't tried the tests either. Theres a FIXME in there...

---
+++ treeedbgen-souffle-tsql/tests/main.rs
@@ -0,0 +1,35 @@
+use std::process::Command;
+
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use tempfile::NamedTempFile;
+
+#[test]
+fn gen() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("treeedbgen-souffle-tsql")?;
+    let tmp = NamedTempFile::new()?;
+    cmd.arg("-o").arg(tmp.path());
+    cmd.arg("--prefix=java").arg("--printsize");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::is_empty())
+        .stderr(predicate::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("treeedb-tsql")?;
+    cmd.arg("tests/tsql/test.sql");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::is_empty())
+        .stderr(predicate::str::is_empty());
+
+    /* TODO FIXME
+    let mut souffle = Command::new("souffle");
+    souffle.arg(tmp.path());
+    souffle
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("java_node\t24"))
+        .stderr(predicate::str::is_empty());
+    Ok(())
+    */
+}
